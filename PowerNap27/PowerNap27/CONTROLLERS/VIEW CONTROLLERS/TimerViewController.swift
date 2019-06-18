1be78b90e2e2a78d0bd35c72664b0e1cf46ee064
Austin West <korruptedanomaly@gmail.com>
2019-06-18 10:52:42 -0600

    Views laid out and TimerViewController started

---
+++ PowerNap27/PowerNap27/CONTROLLERS/VIEW CONTROLLERS/TimerViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TimerViewController.swift
+//  PowerNap27
+//
+//  Created by Austin West on 6/18/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var napButton: UIButton!
+    
+    let timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    
+    
+    @IBAction func napButtonTapped(_ sender: Any) {
+    }
+    
+    func updateLabel() {
+        
+    }
+    
+    func updateButton() {
+        
+    }
+    
+    func startTimer() {
+        
+    }
+    
+    func stopTImer() {
+        
+    }
+    
+    func setTimer() {
+        
+    }
+
+}
